04c8c07a7688f8613f8c3c55aa281388895ab367
takuya3211 <takuya3211@gmail.com>
2014-12-01 15:23:44 +0900

    Initial Commit

---
+++ Lesserpyon3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lesserpyon3
+//
+//  Created by 伊藤 拓也 on 2014/12/01.
+//  Copyright (c) 2014年 Takuya Ito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
